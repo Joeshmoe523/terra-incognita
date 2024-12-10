6e203bf5d215705136687468308d9414e7c9dd58
Joseph Yao <josephyao@Josephs-MacBook-Pro.local>
2024-12-10 15:58:59 -0800

    Added GrowthPlan model to app, not linked to User yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_04_005952) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_10_233017) do
+  create_table "growth_plans", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.text "impact"
+    t.string "macro_skill"
+    t.string "micro_skill"
+    t.text "success_criteria"
+    t.string "focus_area"
+    t.string "category"
+    t.string "scope"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
